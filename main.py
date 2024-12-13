8026528f36c79711f7f57818a1a4874b06aa29ec
Gaurav Agarwal <gagarwal003@gmail.com>
2024-12-12 17:40:59 -0800

    Updated main.py to keep track of prices everyday

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+from datetime import datetime
 
 def clean_and_convert_dict(d):
     cleaned_dict = {}
@@ -30,8 +31,22 @@ for setName in rarityProbabilitiesBySet:
                         row['extRarity']]
     
     pricesBySet[setName] = round(sum,2)
+    
+current_date = datetime.now().strftime('%Y-%m-%d')
+csv_filename = "sv_packs_expected_value.csv"
+file_exists = False
+try:
+    with open(csv_filename, mode='r', newline='') as file:
+        file_exists = True
+except FileNotFoundError:
+    pass
 
-pricesBySet = dict(sorted(pricesBySet.items(), key=lambda item: item[1]))
+with open(csv_filename, mode='a', newline='') as file:
+    writer = csv.writer(file)
 
-for set in pricesBySet:
-    print(f"{set}: ${pricesBySet[set]}")
+    if not file_exists:
+        header = ['Date'] + list(pricesBySet.keys())
+        writer.writerow(header)
+
+    row = [current_date] + list(pricesBySet.values())
+    writer.writerow(row)
